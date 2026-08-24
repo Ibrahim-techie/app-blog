11b899a3cecdfd63ac48b10fc65dbe04abe3ae3f
Ibrahim-techie <ibrahimhatodwala@gmail.com>
2026-08-24 16:15:08 +0530

    feat: created Page AddPost

---
+++ src/Pages/AddPost.jsx
@@ -0,0 +1,13 @@
+import { Container, Postform } from "../components";
+
+function AddPost() {
+  return (
+    <div className="py-8">
+      <Container>
+        <Postform />
+      </Container>
+    </div>
+  );
+}
+
+export default AddPost;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Container from "./container/container";
+import Container from "./container/Container";
 import Logoutbtn from "./header/Logoutbtn";
 import Logo from "./Logo";
 import Header from "./header/Header";
@@ -6,5 +6,25 @@ import Footer from "./footer/Footer";
 import Button from "./Button";
 import Input from "./Input";
 import RTE from "./RTE";
-import Select from "./Select"
-export {Container,Logoutbtn,Logo,Header,Footer,Button,Input,Select,RTE};
+import Select from "./Select";
+import Postcard from "./PostCard";
+import Authlayout from "./AuthLayout";
+import Postform from "./Postform/Postform";
+import Login from "./Login";
+import Signup from "./Signup";
+export {
+  Container,
+  Logoutbtn,
+  Logo,
+  Header,
+  Footer,
+  Button,
+  Input,
+  Select,
+  RTE,
+  Postcard,
+  Authlayout,
+  Postform,
+  Login,
+  Signup,
+};
