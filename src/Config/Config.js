4773fb7fd668a5b7f1524d6f29846af99f150210
Ibrahim Hatod Wala <ibrahimhatodwala@gmail.com>
2026-09-20 23:52:47 +0530

    tweaks for deployment

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,7 +1,7 @@
 const config = {
   appwriteProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-  tableId: import.meta.env.VITE_APPWRITE_Table_ID,
+  tableId: import.meta.env.VITE_APPWRITE_TABLE_ID,
   bucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
   endPoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
   TinyMCE:import.meta.env.VITE_TINYMCE_API_KEY
